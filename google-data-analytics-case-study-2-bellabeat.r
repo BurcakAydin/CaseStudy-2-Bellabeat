4015bae17b307bb863e395a755dee1fcc79932cc
Burçak Aydın <76750399+BurcakAydin@users.noreply.github.com>
2023-02-06 16:24:14 +0300

    Kaggle Notebook | Google Data Analytics: Case Study 2 - Bellabeat | Version 1

---
+++ google-data-analytics-case-study-2-bellabeat.r
@@ -0,0 +1,64 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "27be1d09",
+   "metadata": {
+    "_execution_state": "idle",
+    "_uuid": "051d70d956493feee0c6d64651c6a088724dca2a",
+    "papermill": {
+     "duration": 0.00213,
+     "end_time": "2023-02-06T13:24:03.706283",
+     "exception": false,
+     "start_time": "2023-02-06T13:24:03.704153",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "## This R environment comes with many helpful analytics packages installed\n",
+    "# It is defined by the kaggle/rstats Docker image: https://github.com/kaggle/docker-rstats\n",
+    "# For example, here's a helpful package to load\n",
+    "\n",
+    "library(tidyverse) # metapackage of all tidyverse packages\n",
+    "\n",
+    "# Input data files are available in the read-only \"../input/\" directory\n",
+    "# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory\n",
+    "\n",
+    "list.files(path = \"../input\")\n",
+    "\n",
+    "# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using \"Save & Run All\" \n",
+    "# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.0.5"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 3.550225,
+   "end_time": "2023-02-06T13:24:03.830161",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2023-02-06T13:24:00.279936",
+   "version": "2.4.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
